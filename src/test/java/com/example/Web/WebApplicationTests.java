c10fee42fa93fec64454fc13c752120612f6c579
hosszugyula <hosszu87@gmail.com>
2021-12-01 12:39:25 +0100

    initial commit

--- src/test/java/com/example/Web/WebApplicationTests.java
+++ src/test/java/com/example/Web/WebApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.CMS;
+package com.example.Web;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CmsApplicationTests {
+class WebApplicationTests {
 
 	@Test
 	void contextLoads() {
